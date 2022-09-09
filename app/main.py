e4e9896f361da0bf8e70eb6832e6f1af7718cbde
Daniel Nuske <dnuske@gmail.com>
2022-09-09 04:35:27 -0300

    .

--- app/main.py
+++ app/main.py
@@ -16,13 +16,15 @@ from app.scheduler import schedule_manager
 from app.scheduler import dispatch_manager
 
 import logging
-from ddtrace import config, patch
-patch(logging=True, fastapi=True)
-
+from ddtrace import config, patch_all
+patch_all(logging=True)
 
 config.env = "local"      # the environment the application is in
 config.service = "cronhooks"  # name of your application
 config.version = "0.1"  # version of your application
+
+config.fastapi['service_name'] = 'custom-service-name'
+config.fastapi['request_span_name'] = 'custom-request-span-name'
 
 FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
           '[dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
